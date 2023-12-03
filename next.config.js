8a465d2b3de3076a6d907a63acc347c36e65d838
Sougata <sougatadas9874@gmail.com>
2023-12-03 11:33:47 +0530

    downgrade to version 13.5

--- next.config.js
+++ next.config.js
@@ -1,18 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: "/all-posts",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "no-store, no-cache",
-          },
-        ],
-      },
-    ];
+  experimental: {
+    serverActions: true,
+    mdxRs: true,
+    serverComponentsExternalPackages: ["mongoose"],
   },
 };
 
