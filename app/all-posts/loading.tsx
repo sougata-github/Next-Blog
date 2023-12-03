83b4b87deb01b07f6d2407240981b52141670b8a
Sougata <sougatadas9874@gmail.com>
2023-12-03 21:35:02 +0530

    change loading indicator color.

--- app/all-posts/loading.tsx
+++ app/all-posts/loading.tsx
@@ -1,7 +1,7 @@
 const loading = () => {
   return (
     <div className="px-6 py-40">
-      <h1 className="text-3xl font-bold text-gray-400 md:text-4xl">
+      <h1 className="text-3xl font-bold text-gray-200 md:text-4xl">
         Loading...
       </h1>
     </div>
