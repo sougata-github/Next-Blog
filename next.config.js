265af0e117c695f7a6da5f068d9971b25ede6e86
Sougata <sougatadas9874@gmail.com>
2023-12-03 11:17:52 +0530

    add cache control in next config and remove unused icon from app.

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
         headers: [
           {
             key: "Cache-Control",
-            value: "s-maxage=1, stale-while-revalidate=59",
+            value: "no-store, no-cache",
           },
         ],
       },
